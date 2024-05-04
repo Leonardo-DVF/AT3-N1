4b4aad2237e0ddafa41f6e3e9c53bf1ffa9f44a2
RuanArth <128199906+RuanArth@users.noreply.github.com>
2024-05-04 18:24:51 -0300

    Adicionando conteúdo da classe reclamação

--- src/reservaquartos/ClassReclamacao.java
+++ src/reservaquartos/ClassReclamacao.java
@@ -1,5 +1,22 @@
 package reservaquartos;
 
 public class ClassReclamacao {
+    private String nomeHospede, descricacaoReclamacao;
+
+    public String getNomeHospede() {
+        return nomeHospede;
+    }
+
+    public void setNomeHospede(String nomeHospede) {
+        this.nomeHospede = nomeHospede;
+    }
+
+    public String getDescricacaoReclamacao() {
+        return descricacaoReclamacao;
+    }
+
+    public void setDescricacaoReclamacao(String descricacaoReclamacao) {
+        this.descricacaoReclamacao = descricacaoReclamacao;
+    }
    
 }
